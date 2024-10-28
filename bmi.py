61c8e71d7739e974ce6b0057d963efcc948357f4
Khin Myat Myat Min <khinmyat.23@ichat.sp.edu.sg>
2024-10-28 14:53:43 +0800

    modified returning values

--- bmi.py
+++ bmi.py
@@ -5,9 +5,19 @@ def calculate_bmi(height, weight):
     bmi = weight/(height * height) 
     print("BMI = " + str(bmi))   
 
-    if(bmi < 18.5): print("Under Weight")
-    if(bmi > 25.0): print("Over Weight")
-    else: print("Normal Weight")
+    if(bmi < 18.5): 
+        print("Under Weight")
+        value = -1
+
+    if(bmi > 25.0): 
+        print("Over Weight")
+        value = 1
+    
+    else: 
+        print("Normal Weight")
+        value = 0
+
+    return value
 
 #calculate_bmi(weight="57",height="1.73")
 
